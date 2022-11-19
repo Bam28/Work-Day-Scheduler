7d417dcbd873d9cb3a55f825bcbe7a2966c1bfe7
Luis Jimenez <luisjimenez@Luiss-Air.attlocal.net>
2022-11-18 23:03:34 -0600

    worked on styling

--- script.js
+++ script.js
@@ -4,11 +4,6 @@ const signUpButton = document.querySelector('#sign-up-button');
 const signInButton = document.querySelector('#sign-in-button');
 const daySectionElement = document.querySelector('.hours-section')
 
-signUpButton.addEventListener('click', function signUp(event) {
-    event.preventDefault()
-    signUpForm.classList.add('hide')
-    signInForm.classList.remove('hide')
-});
 
 let todayDate = () => {
     let todayDateFormat = moment().format("MMMM Do YYYY");
@@ -20,33 +15,83 @@ let todayDate = () => {
 
 const dayScheduler = {
     dayCreatorPerHour() {
-        let hoursArray = ['12:00 AM', '1:00 AM', '2:00 AM', '3:00 AM', '4:00 AM', '5:00 AM', '6:00 AM', '7:00 AM', '8:00 AM', '9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM', '6:00 PM', '7:00 PM', '8:00 PM', '9:00 PM', '10:00 PM', '11:00 PM']
+        let hoursArray = ['9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM']
         for(let i = 0; i < hoursArray.length; i++){
-            let perHourInput = document.createElement('input')
-            let perHourLabel = document.createElement('label')
+            let perHourSection = document.createElement('section');
+            let perHourForm = document.createElement('form');
+            let perHourInput = document.createElement('input');
+            let perHourLabel = document.createElement('label');
+            let perHourButton = document.createElement('input');
             let hourText = hoursArray[i]
-            perHourInput.type = 'text'
-            perHourLabel.class = 'hour form-label'
-            perHourLabel.innerHTML = hourText
-            daySectionElement.appendChild(perHourLabel)
-            daySectionElement.appendChild(perHourInput)
+            perHourSection.classList.add('perHourSection');
+            // perHourSection.setAttribute('id', `${i + 1}`);
+            perHourInput.type = 'text';
+            perHourButton.type = 'button';
+            perHourButton.classList.add('perHourButton');
+            perHourLabel.classList.add('hour');
+            perHourLabel.innerHTML = hourText;
+            perHourButton.value = 'Save';
+            daySectionElement.appendChild(perHourSection);
+            perHourSection.appendChild(perHourForm)
+            perHourForm.appendChild(perHourLabel);
+            perHourForm.appendChild(perHourInput);
+            perHourForm.appendChild(perHourButton);
         }
     },
     currentHourColor() {
-        if (currentHour) {
-            addClass("present");
+        let currentHour = moment().format("HH");
+        let hoursArray = ['9:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '1:00 PM', '2:00 PM', '3:00 PM', '4:00 PM', '5:00 PM']
+        if (currentHour > hoursArray) {
+            addClass("past");
             addClass("white-text");
-        } else if (currentHour) {
+        } else if (currentHour = id) {
             removeClass("present");
             addClass("future");
-        } else if (currentHour) {
+        } else if (currentHour < id) {
             removeClass("future");
             addClass("past");
         }
-      }
+    },
+    saveNote(note) {
+        let key = note.substring(0, 3);
+        localStorage.setItem(key, note);
+    }
 }
+
 
 todayDate()
 dayScheduler.dayCreatorPerHour()
+dayScheduler.currentHourColor()
 
 
+/*
+function makeColorCode()
+{
+        timerInterval = setInterval(function()
+    {
+            for(t=0;t<timeofDayCount;t++)
+        {
+            var colouredTextA =$("#"+timeofDayCounter);
+            var timeForNow = moment();
+            var timeForNow2=timeForNow.format("HH");
+            var slotTime = moment(timeOfDay[t],"HH mm a");
+            if(timeForNow2==timeofDayCounter)
+            {
+                colouredTextA.addClass("present");
+            }
+            else
+            {
+                if(timeForNow.isAfter(slotTime))
+                {
+                    colouredTextA.addClass("past");
+                }
+                else
+                {
+                    colouredTextA.addClass("future");
+                }
+            }
+            timeofDayCounter++;
+        }
+    }, 100);
+}
+*/
